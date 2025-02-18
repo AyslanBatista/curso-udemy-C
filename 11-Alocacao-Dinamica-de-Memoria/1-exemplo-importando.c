cd282c9a2981e8a41172d4b2832a25ec6822b09a
AyslanBatista <ayslan.batista@hotmail.com>
2025-02-18 02:48:54 -0300

    Alocao Dinamica de Memoria

---
+++ 11-Alocacao-Dinamica-de-Memoria/1-exemplo-importando.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//----Importando um arquivo----//
+// #include "functions.h"
+
+int main()
+{
+
+    // Chama a função
+    imprimeOi();
+
+    // Retorno da Função
+    return 0;
+}
+
+// ----------OUTRO ARQUIVO ABAIXO----------- //
+// functions.h
+
+void imprimeOi()
+{
+    printf("Oi\n");
+}
